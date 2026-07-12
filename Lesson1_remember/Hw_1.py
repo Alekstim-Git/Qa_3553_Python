2688558205432c93948b4c6bd0635f961c5cffe1
AleksPav <alekstimov@gmail.com>
2026-07-12 10:29:05 +0300

    Lesson_1 Qa

--- Lesson1_remember/Hw_1.py
+++ Lesson1_remember/Hw_1.py
@@ -8,13 +8,12 @@ def print_string_reverse(s):
     else:
         print(s[::-1])
 
-
 print_string_reverse('Shalom')
 print_string_reverse('      ')
+print_string_reverse('')
 
 # № 2
 print()
-
 
 def is_isr_phone_number(phone):
     if phone is None or phone == '' or phone.isspace():
@@ -24,14 +23,14 @@ def is_isr_phone_number(phone):
     else:
         return False
 
-
 print(is_isr_phone_number('0549856345'))
 print(is_isr_phone_number('154985634r'))
+print(is_isr_phone_number('1549856349'))
 print(is_isr_phone_number(''))
+print(is_isr_phone_number(' '))
 
 # № 3
 print()
-
 
 def print_substring_reverse(s, start, finish):
     if s is None or s == '' or s.isspace():
@@ -46,5 +45,6 @@ def print_substring_reverse(s, start, finish):
     result = start_part + middle_part[::-1] + end_part
     print(result)
 print_substring_reverse("Shalom", 1, 3)
-# № 4
-print()
+print_substring_reverse(None, 1, 3)
+print_substring_reverse("Hi", 3, 2)
+
