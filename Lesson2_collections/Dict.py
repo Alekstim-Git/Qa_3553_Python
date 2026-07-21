67ae2ef0712f006d909d30d5d869bc368c7fa656
AleksPav <alekstimov@gmail.com>
2026-07-21 21:26:56 +0300

    Lesson_4_Qa_Hw_4

--- Lesson2_collections/Dict.py
+++ Lesson2_collections/Dict.py
@@ -4,7 +4,7 @@ books = {
 }
 
 print()
-books1 = {  # в set -> порядок элементов всегда случайный
+books1 = {                           # в set -> порядок элементов всегда случайный
     'Harry Potter and.....',
     'To kill a mockingdird'
 }
@@ -15,17 +15,17 @@ response = {
     'status': 'success',
     'user': {'id': 1, 'name': 'Maria'}
 }
-print(response['user']['name'])  # вытащили имя
+print(response['user']['name'])         # вытащили имя
 print()
 
 data = [1, 22, 3]
-print(isinstance(data, list))  # метод показывает list ли это
+print(isinstance(data, list))        # метод показывает list ли это
 
-value = 10  # провер int-True или float-False лежит
+value = 10                            # провер int-True или float-False лежит
 print(isinstance(value, (int,)))
 print()
 
-team_ages = {  # что стоит слева, то key / что справа, то value !!
+team_ages = {                      # что стоит слева, то key / что справа, то value !!
     'Alex': 23,
     'Viki': 42,
     'Petr': 52
@@ -34,7 +34,7 @@ print(team_ages.keys())
 print(team_ages.values())
 print()
 
-team_names = ['Alex', 'Viki', 'Petr']  # ZIP собирает словарь
+team_names = ['Alex', 'Viki', 'Petr']        # ZIP собирает словарь
 team_numbers = [23, 43, 52]
 team_ages = {name: age for name, age in zip(team_names, team_numbers)}
 print(team_ages)
